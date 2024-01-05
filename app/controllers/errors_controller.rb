a39c0e6c01a9498086165ab65cc9d2a50f5aaba8
hschne <hans.schnedlitz@gmail.com>
2024-01-05 21:31:34 +0100

    Add CI, add errors controller

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class ErrorsController < ApplicationController
+  def not_found
+    respond_to do |format|
+      format.html { render status: :not_found }
+      format.json { render json: { error: "not_found" }, status: :not_found }
+    end
+  end
+
+  def internal_server_error
+    respond_to do |format|
+      format.html { render status: :internal_server_error }
+      format.json { render json: { error: "server_error" }, status: :internal_server_error }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root "application#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', as: :rails_health_check
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Preview error pages
+  get "/errors/404", to: "errors#not_found"
+  get "/errors/500", to: "errors#internal_server_error"
+
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 
   # Defines the root path route ("/")
   # root "posts#index"
