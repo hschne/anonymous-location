52edffdf51c65fd12cf5f0cd0b8b4b42a34e6e34
hschne <hans.schnedlitz@gmail.com>
2024-01-10 17:18:35 +0100

    Add map

--- app/javascript/controllers/maps_controller.js
+++ app/javascript/controllers/maps_controller.js
@@ -3,19 +3,36 @@ import L from "leaflet";
 
 // Connects to data-controller="maps"
 export default class extends Controller {
-  static targets = ["container"];
+  static targets = ["container", "mapContainer"];
 
   connect() {
-    var map = L.map(this.containerTarget).setView([48.210033, 16.363449], 12);
-
-    L.tileLayer(
+    this.map = L.map(this.mapContainerTarget);
+    const tiles = L.tileLayer(
       "https://api.maptiler.com/maps/basic-v2/{z}/{x}/{y}.png?key=CV0ZSLFSeTgHr5Pq8Y6U",
       {
         maxZoom: 19,
         attribution:
           '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       },
-    ).addTo(map);
+    );
+    this.map.addLayer(tiles);
+    this.map
+      .locate({
+        setView: true,
+        maxZoom: 19,
+        enableHighAccuracy: true,
+        maximumAge: 0,
+      })
+      .on("locationerror", this.onLocationError);
+  }
+
+  setLocation() {}
+
+  onLocationError(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+    // Hack, properly remove map
+    document.querySelector("#map").innerHTML =
+      `ERROR(${err.code}): ${err.message}`;
   }
 
   disconnect() {
