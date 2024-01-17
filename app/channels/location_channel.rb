779e68fbd6591686ddacaecf03a4d6a0333aae1f
hschne <hans.schnedlitz@gmail.com>
2024-01-17 11:05:32 +0100

    Add initial location loading

--- app/channels/location_channel.rb
+++ app/channels/location_channel.rb
@@ -1,13 +1,13 @@
 class LocationChannel < ApplicationCable::Channel
   def subscribed
     location = Location.find_by!(key: params[:key])
-    location.increment!(:location_count)
-    stream_for(location)
+    location.increment!(:client_count)
+    stream_for("location")
   end
 
   def unsubscribed
     location = Location.find_by!(key: params[:key])
-    location.decrement!(:location_count)
+    location.decrement!(:client_count)
   end
 
   def receive(data)
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,18 +2,64 @@ import { Controller } from "@hotwired/stimulus";
 import consumer from "../channels/consumer";
 
 export default class extends Controller {
-  static targets = ["locationInput"];
+  static values = {
+    key: String,
+    client: String,
+    location: String,
+  };
 
   connect() {
     maptilersdk.config.apiKey = "CV0ZSLFSeTgHr5Pq8Y6U";
 
-    const map = new maptilersdk.Map({
+    this.connectToChannel();
+  }
+
+  connectToChannel() {
+    this.subscription = consumer.subscriptions.create(
+      {
+        channel: "LocationChannel",
+        key: this.keyValue,
+      },
+      {
+        connected: this._connected.bind(this),
+        disconnected: this._disconnected.bind(this),
+        received: this._received.bind(this),
+      },
+    );
+  }
+
+  _connected({ reconnected }) {
+    console.log("Connected...");
+    if (reconnected) {
+      console.log("Reconnected...");
+    }
+    this.initializeMap();
+  }
+
+  _disconnected() {
+    console.log("Disconnected...");
+    const event = new CustomEvent("channel-disconnected");
+    window.dispatchEvent(event);
+  }
+
+  _received(data) {
+    console.log(data);
+
+    console.log("Location updated...");
+    const event = new CustomEvent("location-changed");
+    window.dispatchEvent(event, data);
+  }
+
+  initializeMap() {
+    this.map = new maptilersdk.Map({
       container: this.element,
+      center: this.locationValue.split(","),
+      zoom: 15,
       style: maptilersdk.MapStyle.BASIC,
-      geolocate: maptilersdk.GeolocationType.POINT,
       geolocateControl: false,
       navigationControl: false,
     });
+    this.addLocationMarker();
 
     const options = {
       enableHighAccuracy: false,
@@ -21,68 +67,41 @@ export default class extends Controller {
       maximumAge: 0,
     };
     navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      this.error,
+      (position) => {
+        this.sendPosition(position);
+        this.addClientMarker(position);
+      },
+      (err) => this.error(err),
       options,
     );
-    this.connectToChannel();
   }
 
-  updatePosition(pos) {
+  addLocationMarker() {
+    const marker = new maptilersdk.Marker({
+      color: "#fff",
+      draggable: false,
+    }).setLngLat(this.locationValue.split(","));
+    marker.addTo(this.map);
+  }
+
+  addClientMarker(pos) {
+    console.log(pos);
     console.log(pos.coords);
-    this.locationChannel.send({
-      sent_by: "Paul",
-      body: "This is a cool chat app.",
-    });
+    const marker = new maptilersdk.Marker({
+      color: "#ff0000",
+      draggable: false,
+    }).setLngLat([pos.coords.longitude, pos.coords.latitude]);
+    marker.addTo(this.map);
   }
 
-  connectToChannel() {
-    this.locationChannel = consumer.subscriptions.create(
-      {
-        channel: "LocationChannel",
-        key: this.keyValue,
-      },
-      {
-        connected() {
-          console.log("Connected...");
-        },
-        disconnected() {
-          console.log("Disconnected...");
-          const event = new CustomEvent("channel-disconnected");
-          window.dispatchEvent(event);
-        },
-        received(data) {
-          console.log("Location updated...");
-          const event = new CustomEvent("location-changed");
-          window.dispatchEvent(event, data);
-        },
-      },
-    );
+  sendPosition(pos) {
+    this.subscription.send({
+      client: this.clientValue,
+      location: pos.coords,
+    });
   }
 
   error(err) {
     console.error(`ERROR(${err.code}): ${err.message}`);
   }
 }
-
-// export default class extends Controller {
-//   connect() {
-//     console.log("Map controller connected...");
-//     maptilersdk.config.apiKey = "CV0ZSLFSeTgHr5Pq8Y6U";
-//     this.map = new maptilersdk.Map({
-//       container: this.element,
-//       style: maptilersdk.MapStyle.BASIC,
-//       geolocate: maptilersdk.GeolocationType.POINT,
-//       geolocateControl: false,
-//       navigationControl: false,
-//     });
-//
-//   }
-//
-//
-//   error(err) {
-//     console.error(`ERROR(${err.code}): ${err.message}`);
-//   }
-//
-//   }
-// }
