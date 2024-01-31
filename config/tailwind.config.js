a5eee6d66a66b6121908d45dc6dbe2da295dae62
hschne <hans.schnedlitz@gmail.com>
2024-01-31 12:43:17 +0100

    Homepage styling

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,22 @@ module.exports = {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
+      animation: {
+        tilt: "tilt 10s infinite linear",
+      },
+      keyframes: {
+        tilt: {
+          "0%, 50%, 100%": {
+            transform: "rotate(0deg)",
+          },
+          "25%": {
+            transform: "rotate(0.5deg)",
+          },
+          "75%": {
+            transform: "rotate(-0.5deg)",
+          },
+        },
+      },
     },
   },
   plugins: [
